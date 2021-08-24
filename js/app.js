f7f617b42500baa18e9e4d3aad436f948fefb501
Steve Ngo <stevehngo@hotmail.com>
2021-08-24 00:01:18 -0400

    update js

---
+++ js/app.js
@@ -0,0 +1,22 @@
+'use strict';
+
+console.log('hello world');
+
+/*
+need way to display items
+global function for displaying 3 items
+item objects
+items array
+need way to determin which item was selected
+items counter
+items randomizer
+event listen for click
+click handler
+logic to update global counter
+
+// ------------------------ Global Variables ------------------------ //
+// ------------------------- Constructor Function ------------------------ //
+// ------------------------- Prototype things ------------------------ //
+// ------------------------- Global Functions ------------------------ //
+// ------------------------- Listeners ------------------------ //
+// ------------------------- Call Functions -------------------------- //
