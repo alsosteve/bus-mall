784c8ea58a81d65d45bc9bc0b863dd61d2e04681
Steve Ngo <stevehngo@hotmail.com>
2021-08-29 19:20:59 -0400

    update

--- js/app.js
+++ js/app.js
@@ -54,7 +54,7 @@ let rounds = 5;
 function Items(name, image, timesShown, votes) {
   this.name = name;
   this.image = image;
-  if (timesShown === null) {
+  if (timesShown === NaN) {
     this.timesShown = 0;
     this.votes = 0;
   } else {
